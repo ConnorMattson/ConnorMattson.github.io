e5c134565ace1e4578156a94ac1be698ea0f939f
ConnorMattson <ConnorMattson@users.noreply.github.com>
2018-02-08 17:59:38 +1300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.to-fade').css('display', 'none');
+	$('.to-fade').fadeIn(250);
+
+	$('a').click(function(event) {
+		event.preventDefault();
+		newLocation = this.href;
+		$('.to-fade').fadeOut(250, newpage);
+	});
+
+	function newpage() {
+		window.location = newLocation;
+	}
+
+});
+
+function strikethrough(item) {
+	$(item).css('text-decoration', 'line-through');
+	$(item).css('text-decoration-color', '#981B1E');
+}
