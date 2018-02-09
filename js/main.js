2eae8dd017c105c5fc3ff30eb77a07142295f5ea
ConnorMattson <ConnorMattson@users.noreply.github.com>
2018-02-09 16:09:19 +1300

    Added Favicon and Blog
    Added a prototype for the blog, there are currently only examples on there and the search function isn't implemented yet.

--- js/main.js
+++ js/main.js
@@ -8,9 +8,35 @@ $(document).ready(function() {
 		$('.to-fade').fadeOut(250, newpage);
 	});
 
+
 	function newpage() {
 		window.location = newLocation;
 	}
+
+
+	$('#blog-search').on({
+    mouseenter: function () {
+        clearTimeout($(this).data('timeoutId'));
+	    $('#blog-search').animate({width: '13%'}, 100);
+		$('#blog-search-input').css('display', 'inline');
+		$('#blog-nav').css('margin-top', '0.98vh');
+		$('.blog-search-items img').css('transform', 'translate(50%, 1px)');
+    },
+    mouseleave: function () {
+        var someElement = $(this),
+        timeoutId = setTimeout(function(){
+            $('#blog-search').animate({width: '1.5%'}, 100);
+			$('#blog-search-input').css('display', 'none');
+			$('#blog-nav').css('margin-top', '1vh');
+			$('.blog-search-items img').css('transform', 'translate(25%, 1px)');
+        }, 1000);
+	    //set the timeoutId, allowing us to clear this trigger if the mouse comes back over
+	    someElement.data('timeoutId', timeoutId); 
+    },
+    click: function() {
+    	$('#blog-search-input').focus()
+    }
+});
 
 });
 
@@ -18,3 +44,75 @@ function strikethrough(item) {
 	$(item).css('text-decoration', 'line-through');
 	$(item).css('text-decoration-color', '#981B1E');
 }
+
+
+var currentlySorted = []
+var catgeories = ['ai', 'security', 'operations-research', 'miscellaneous']
+
+function sortBlog(sortingCategory) {
+	// If this is the first category added, dulls out the rest
+	if (currentlySorted.length == 0) {
+		for (var i = 0; i < catgeories.length; i += 1) {
+			$('#blog-category-'+catgeories[i]).css('color', 'lightgray')
+		}
+		$('#blog-category-clearFilter').css('visibility', 'visible')
+		$('#blog-category-clearFilter').fadeTo(200, 1)
+	}
+
+	// Should the category be added or removed from filters?
+	if (currentlySorted.includes(sortingCategory)) {
+		currentlySorted.pop(sortingCategory)
+		$('#blog-category-'+sortingCategory).css('color', 'lightgray')
+		
+	}
+	else {
+		currentlySorted.push(sortingCategory)
+		$('#blog-category-'+sortingCategory).css('color', 'black')
+	}
+
+	// Filters the content
+	if (currentlySorted.length == 0) clearFilter()
+	else {
+		for (var i = 0; i < catgeories.length; i += 1) {
+			if (currentlySorted.includes(catgeories[i])) $('.blog-'+catgeories[i]).fadeIn(250)
+			else $('.blog-'+catgeories[i]).fadeOut(250)
+		}
+	}
+}
+
+function clearFilter() {
+	currentlySorted = []
+	for (var i = 0; i < catgeories.length; i += 1) {
+		$('.blog-'+catgeories[i]).fadeIn(250)
+		$('#blog-category-'+catgeories[i]).css('color', 'lightgray')
+		$('.blog-'+catgeories[i]).fadeIn(250)
+	}
+	$('#blog-category-clearFilter').fadeTo(100, 0)
+}
+
+function fadeInBlog(element) {
+	if (!currentlySorted.includes(element)) $('#blog-category-'+element).css('color', 'black')
+}
+
+function fadeOutBlog(element) {
+	if (!currentlySorted.includes(element)) $('#blog-category-'+element).css('color', 'lightgray')
+}
+
+function clearFilterHover() {
+	for (var i = 0; i < currentlySorted.length; i += 1) {
+		$('#blog-category-'+currentlySorted[i]).css('color', '#981B1E');
+	}
+}
+
+function clearFilterHoverOff() {
+	for (var i = 0; i < currentlySorted.length; i += 1) {
+		$('#blog-category-'+currentlySorted[i]).css('color', 'black');
+	}
+}
+
+var link= document.getElementById('reflectedLink');
+    var input= document.getElementById('blog-search-input');
+    input.onchange=input.onkeyup= function() {
+        link.search= 'search?q='+encodeURIComponent(input.value);
+        link.firstChild.data= link.href;
+    };
