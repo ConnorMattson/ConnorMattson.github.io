754cdf4c9e18dab4d86cff258965802ad3b98488
ConnorMattson <ConnorMattson@users.noreply.github.com>
2018-02-16 14:55:17 +1300

    Added project to Project Page
    Barring some polishing, the project page is now finished. I still need to fill out some detail for Project Make once development is further on.

---
+++ js/projects.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	var modal = document.getElementById('modal');
+	var span = document.getElementsByClassName("close-modal")[0];
+
+	window.onclick = function(event) {
+		if (event.target == modal) closeModal();
+	}
+});
+
+function openModal(toDisplay) {
+	$('#modal').css('display', 'block')
+	$('#modal').fadeTo(250, 1)
+	console.log('projects/' + toDisplay + '.html')
+	document.getElementById('modal-content').src = 'projects/' + toDisplay + '.html'
+}
+
+function closeModal() {
+	$('#modal').fadeTo(250, 0)
+    $('#modal').css('display', 'none')
+}
