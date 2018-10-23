e3f70627d51992a4f86f5bb0d52fdf204405a5c4
ConnorMattson <ConnorMattson@users.noreply.github.com>
2018-10-23 20:52:06 +1300

    Added programming challenges page

--- js/main.js
+++ js/main.js
@@ -20,3 +20,70 @@ function strikethrough(item) {
 	$(item).css('text-decoration', 'line-through');
 	$(item).css('text-decoration-color', '#981B1E');
 }
+
+const getModalData = (function() {
+	let data = [];
+
+	const getData = function(dataName) {return data.filter((element) => element.title == dataName)[0]};
+
+	const xhr = new XMLHttpRequest();
+	let uri = "https://connormattson.github.io/media/projectInfo.json";
+	xhr.open("GET", uri, true);
+	xhr.onload = function() { data = JSON.parse(xhr.responseText).data;	}
+	xhr.send(null);
+
+	return getData;
+})();
+
+function openModal(toDisplay) {
+	$('#modal').css('display', 'flex');
+	$('#modal').fadeTo(250, 1);
+
+	let data = getModalData(toDisplay);
+	document.getElementById('modal-title').innerHTML = data.title;
+	document.getElementById('modal-text').innerHTML = data.content;
+	document.getElementById('modal-image').innerHTML = "<img src='media/projects/" + data.image + "'/>";
+	if (data.hasOwnProperty("link")) {
+		document.getElementById('modal-text').innerHTML += "<br/>";
+		data.link.forEach((link) => document.getElementById('modal-text').innerHTML += "<a href='" + link.url + "' target='blank'><img src='media/projects/" + link.img + "'/></a>");
+	}
+}
+
+function closeModal() {
+	$('#modal').fadeTo(250, 0)
+	setTimeout(function() { $('#modal').css('display', 'none') }, 250)
+}
+
+function filterDownloads() {
+	let filter = document.getElementById("filter").value;
+	let tableContents = "<tr><th> Filename </th><th> Problem Source </th><th> Problem </th><th> Points </th><th> Source code </th><th> Try me! </th></tr>\n";
+	let github = "https://github.com/ConnorMattson/Misc-Scripts-and-Challenge-Solutions/blob/master";
+	let pointValues = ["3", "10", "30", "100"];
+	let allowedPoints = [];
+	pointValues.forEach((element) => { if (document.getElementById(element).checked) allowedPoints.push(element) });
+
+
+	data.filter((element) => (element.filename.includes(filter) || element.problem.includes(filter)) && (allowedPoints.indexOf(element.points) > -1)) 
+			.forEach(function(element) {
+	tableContents += "<tr><td>" + element.filename + "</td>";
+	tableContents += "<td><a href='" + element.problemLink + "'>" + element.problemSource + "</a></td>";
+	tableContents += "<td>" + element.problem + "</td>";
+	tableContents += "<td>" + element.points + "</td>";
+	tableContents += "<td><a href='" + github + element.githubLink + "'><img src='https://connormattson.github.io/media/projects/GitHub.svg'></a></td>";
+	tableContents += "<td> Coming soon... </td></tr>";
+			});
+	document.getElementById("problemTable").innerHTML = tableContents;
+}
+
+function loadData() {
+	data = []
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://connormattson.github.io/Misc-Scripts-and-Challenge-Solutions/problemData.json", true);
+
+	xhr.onreadystatechange  = (function(response) {
+		data = JSON.parse(xhr.responseText);
+		filterDownloads("")
+	});
+
+	xhr.send(null);
+}
